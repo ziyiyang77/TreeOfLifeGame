f4ab51cfc5ce2e4331d15f53497ec6580c4678b3
HUDongge <1113718802@qq.com>
2024-04-11 10:15:34 +0800

    level2美术合并

---
+++ TreeOfLife/Assets/Scripts/Level 1_2/CameraSwitcher.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public Camera firstCamera; 
+
+    private Camera mainCamera; 
+    private bool isInTriggerZone = false;
+
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        firstCamera.enabled = false; 
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInTriggerZone = true;
+            Debug.Log("OnTriggerEnter");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isInTriggerZone = false;
+            Debug.Log("OnTriggerExit");
+        }
+    }
+
+    void Update()
+    {
+        if (isInTriggerZone && Input.GetKeyDown(KeyCode.F))
+        {
+
+            firstCamera.enabled = true;
+            
+            mainCamera.enabled = false;
+            Debug.Log("first view");
+        }
+       
+
+
+    }
+}
--- TreeOfLife/Assets/Scripts/Level 1_2/RotateLight.cs
+++ TreeOfLife/Assets/Scripts/Level 1_2/RotateLight.cs
@@ -4,12 +4,13 @@ public class RotateLight : MonoBehaviour
 {
     private bool isDragging = false;
     private Vector3 offset;
-    private Transform rodTransform;
+    public Transform rodTransform;
+    public float radius=2.53f;//The plate radius is 2.53f
+   
 
     private void Start()
     {
-        // 获取圆形杆的Transform组件
-        rodTransform = GameObject.Find("guidao").transform;
+        
     }
 
     private void OnMouseDown()
@@ -23,22 +24,22 @@ public class RotateLight : MonoBehaviour
     {
         if (isDragging)
         {
-            // 计算拖动后的位置
+            
             Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)) + offset;
-            // 将位置限制在圆形杆的表面上
+          
             newPosition = LimitToRod(newPosition);
-            // 更新小球的位置
+            
             transform.position = newPosition;
         }
     }
 
     private Vector3 LimitToRod(Vector3 newPosition)
     {
-        // 计算新位置相对于圆形杆中心的向量
+        
         Vector3 direction = newPosition - rodTransform.position;
-        // 将方向向量限制在圆形杆半径上 2.8f是圆的半径长度
-        direction = direction.normalized * 2.5f;
-        // 限制小球位置在圆形杆的表面上
+     
+        direction = direction.normalized * radius; 
+        
         newPosition = rodTransform.position + direction;
         return newPosition;
     }
