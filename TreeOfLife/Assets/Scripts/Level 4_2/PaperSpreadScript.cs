728fc471a07d450c2a324b049da7afdd2e702645
HUDongge <1113718802@qq.com>
2024-04-26 18:01:02 +0800

    factory4-2加剧情

---
+++ TreeOfLife/Assets/Scripts/Level 4_2/PaperSpreadScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PaperSpreadScript : MonoBehaviour
+{
+    public GameObject paperImage; // 引用信纸UI元素  
+    private bool isFullScreen = false; // 标记信纸是否铺满屏幕  
+    public GameObject cue; //F
+    public static bool Isread=false;
+    void Start()
+    {
+        paperImage.SetActive(false);
+        cue.SetActive(false);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Isread = true;
+            cue.SetActive(true);
+          
+            Debug.Log("Enterletter");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            cue.SetActive(false);
+            Isread = false;
+            Debug.Log("Exitletter");
+        }
+    }
+     void Update()
+    {
+       if(Isread&&Input.GetKeyDown(KeyCode.F))
+        {
+            paperImage.SetActive(true);
+        }
+    }
+
+
+}
